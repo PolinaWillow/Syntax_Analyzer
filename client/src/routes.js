53d0eb5812a253a1825c2490c69c9a235da88a93
volovikm <148370088+volovikm@users.noreply.github.com>
2023-10-26 14:47:15 +0300

    Изменены пути

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,9 +7,9 @@ import { Route, Routes, Navigate } from "react-router-dom"
 export const useRoutes = () =>{
     return(
         <Routes>
-            <Route path="/pages/HomePage" exact element={<HomePage/>}/>
-            <Route path="/pages/FormPage" exact element={<FormPage/>}/>
-            <Route path="/pages/ResultPage" exact element={<ResultPage/>}/>
+            <Route path="/" exact element={<HomePage/>}/>
+            <Route path="/FormPage" exact element={<FormPage/>}/>
+            <Route path="/ResultPage" exact element={<ResultPage/>}/>
             <Route path="*" element={<Navigate to="/" replace />}/>             
         </Routes>
     );
