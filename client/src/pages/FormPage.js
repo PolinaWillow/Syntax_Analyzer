4f47a46b53e9edc3980a2f0129d1229b0dc92604
Polina <polianna_654@mail.ru>
2023-10-29 08:20:22 +0300

    Сбор данных с формы и исправление багов в верствке

--- client/src/pages/FormPage.js
+++ client/src/pages/FormPage.js
@@ -1,67 +1,99 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Navbar} from '../components/navbar.js'
 import {Button} from '../components/button.js'
 import {Backward} from '../components/backward.js'
 import {useNavigate} from "react-router-dom";
 
+import { useHttp } from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook'
+
 export const FormPage = () => {
     const navigate = useNavigate();
+
+    const {loading, request, error, clearError} = useHttp()
+
+    //Отслеживание ошибок
+    const message = useMessage()
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
+
+
+    const [form, setForm] = useState({
+        text:"", file: "", typeAnalyze: "", partial: "", keyWord: ""
+    })
+
+    const ChangeHandler = event =>{
+        setForm({...form, [event.target.name]:event.target.value})
+
+        console.log(form)
+    }
+
+    const SendForm = async()=>{
+        try {
+            const data = await request('Наш сервер', 'POST', {...form})
+            console.log('Data', data) 
+        } catch (error) {
+
+        }
+    }
 
     return(
         <>
         <Navbar></Navbar>
-        <div class="main-space" align="center">
-            <form enctype="multipart/form-data" action="" method="POST">
-            <div class="side" align="left">
-                <label class='settings-block-label'>Введите текст для анализа</label>
-                <textarea name="text"></textarea>
-                <div>
-                <label class="file-input-label" for="text_file">Загрузить исходный файл</label>
-                <input id="text_file" type="file" name="text_file"/>
+        <div className="main-space" align="center">
+            <form encType="multipart/form-data" action="" method="POST">
+                <div className="side" align="left">
+                    <h3>Введите текст для анализа</h3>
+                    <textarea name="text" onChange={ChangeHandler}></textarea>
+                    <div>
+                        <label htmlFor="text_file" className="file-input-label">Загрузить исходный файл</label>
+                        <input id="text_file" type="file" name="file" onChange={ChangeHandler}/>
+                    </div>
                 </div>
-            </div>
 
-            <div class="side" align="right">
-                <div class="settings-div" align="left">
-                <label class='settings-block-label'>Настройки</label>
-                <div class='settings-par'>
-                    <input type="radio" name="full_analysis"/>
-                    <label class="settings-label">Полный анализ</label>
-                </div>
-                <div class='settings-par'>
-                    <input type="radio" name="partial_analysis"/>
-                    <label class="settings-label">Анализ по членам предложениям</label>
-                </div>
-                <div class="settings-choice">
-                    <div class="settings-choice-inline">
-                    <div class='settings-choice-part settings-par'>
-                        <input type="checkbox" name="partial_subject"/>
-                        <label class="settings-label">Подлежащее</label>
-                    </div>
-                    <div class='settings-choice-part settings-par'>
-                        <input type="checkbox" name="partial_predicate"/>
-                        <label class="settings-label">Сказуемое</label>
-                    </div>
-                    </div>
-                    <div class="settings-choice-inline">
-                        <div class='settings-choice-part settings-par'>
-                            <input type="checkbox" name="partial_object"/>
-                            <label class="settings-label">Дополнение</label>
+                <div className="side" align="right">
+                    <div className="settings-div" align="left">
+                        <h3>Настройки</h3>
+                        <div className='settings-par'>
+                            <input type="radio" value={"full_analysis"} name="typeAnalyze" onChange={ChangeHandler}/>
+                            <label htmlFor="typeAnalyze" className="settings-label" >Полный анализ</label>
+                        </div>
+                        <div className='settings-par'>
+                            <input type="radio" value={"partial_analysis"} name="typeAnalyze" onChange={ChangeHandler}/>
+                            <label htmlFor="typeAnalyze" className="settings-label">Анализ по членам предложениям</label>
+                        </div>
+                        <div className="settings-choice">
+                            <div className="settings-choice-inline">
+                            <div className='settings-choice-part settings-par'>
+                                <input type="checkbox" value={"subject"} name="partial" onChange={ChangeHandler}/>
+                                <label className="settings-label">Подлежащее</label>
+                            </div>
+                            <div className='settings-choice-part settings-par'>
+                                <input type="checkbox" value={"predicate"} name="partial" onChange={ChangeHandler}/>
+                                <label className="settings-label">Сказуемое</label>
+                            </div>
+                            </div>
+                            <div className="settings-choice-inline">
+                                <div className='settings-choice-part settings-par'>
+                                    <input type="checkbox" value={"predicate"} name="partial" onChange={ChangeHandler}/>
+                                    <label className="settings-label">Дополнение</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='settings-par'>
+                            <input type="radio" value={"word_analysis"} name="typeAnalyze" onChange={ChangeHandler}/>
+                            <label className="settings-label">Анализ по слову:</label>
+                        </div>
+                        <input className="input-word" name="word"/>
+                        <div align="center">
+                            <div className="button-submit">
+                                <Button onClick={SendForm} size="sm" text="Анализировать"/> 
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class='settings-par'>
-                    <input type="radio" name="word_analysis"/>
-                    <label class="settings-label">Анализ по слову:</label>
-                </div>
-                <input class="input-word" name="word"/>
-                <div align="center">
-                    <div class="button-submit">
-                    <Button onClick={() => navigate('/ResultPage')} size="sm" text="Анализировать"/> 
-                    </div>
-                </div>
-                </div>
-            </div>
 
             </form>
         </div>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -16,13 +16,11 @@ export const HomePage = () => {
                 Анализатор позволяет сделать полный синтаксический разбор предложения, найти определенные члены предложения, а так же выделить предложения, в которых выбранным членом предложения является определенное слово
             </p>
             </div>
-            <Button onClick={() => navigate('/FormPage')} size="lg" text="Анализировать"/> 
-            <footer>
-            <div align="left">
-                &nbsp;2023 - Бригада
-            </div>
-            </footer>
+            <Button onClick={() => navigate('/FormPage')} size="lg" text="Начать анализ"/> 
         </div>
+        <footer align="left">
+                &nbsp;2023 - Бригада
+        </footer>
         </>
     )
 }
