10fffb3f91a136a698dff3d3d9951bbc463c9e95
Polina <polianna_654@mail.ru>
2023-11-14 13:17:40 +0300

    Сервер на python

--- server/index.py
+++ server/index.py
@@ -1,5 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+#Модель для входных данных
+class Item(BaseModel):
+    text: str
+    file: str | None = None #Исправить на соответствующий тип данных
+    typeAnalyzer: str
+    #partial: [] Разобраться с типом данных для массива
+    word: str | None = None
 
 app = FastAPI()
 
@@ -7,3 +16,9 @@ app = FastAPI()
 def read_root():
     print ("Hello world")
     return {"Hello": "World"}
+
+@app.post("/start_analyze")
+def create_item(item: Item):
+    print(item)
+    #Добавить обработчик для анализа, запись в бд и отправку файла с результатами анализа
+    return {"message": "Analyze"}
